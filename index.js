60a60377049e1a4222aa7f343fefafcca63fe307
Subhadeep-pan <subhadeeppan047@gmail.com>
2025-04-13 12:00:08 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV, hey i am here')
+  res.send('Hello MCKV, I am subhadeep')
 })
 
 app.get('/courses', function (req, res) {
